b5213728806b54ce0f956f4248e4327cac743b55
MahmodNabwy <mahmoud.ashraf@digitalhub.com.eg>
2023-12-12 11:46:38 +0200

    Finish Courses Card  in Normal Figure

--- src/Components/Statistics/Courses/CourseProgress.tsx
+++ src/Components/Statistics/Courses/CourseProgress.tsx
@@ -258,33 +258,53 @@ const CourseProgress = (props: CourseProgressProps) => {
     //Normal Figure
     <>
       <div
-        className="grid grid-cols-1 xs:grid-col-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2  card-section"
-        style={{ height: "263px" }}
+        className="grid grid-cols-1 xs:grid-col-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-1 courses-section"
+        style={{ gridTemplateRows: "auto auto auto auto", height: "263px" }}
       >
-        <MainCard
-          cardId={1}
-          expanded={true}
-          icon={coursesIcon}
-          title="Courses"
-        />
-        <div className="col-start-1 col-end-1 xs:col-start-1 sm:col-start-1 pr-4 pl-4">
-          <div className="flex course-status">Latest Course</div>
+        <div className="col-span-12 row-span-1">
+          <MainCard
+            icon={coursesIcon}
+            title="Courses"
+            cardId={1}
+            expanded={true}
+          />
         </div>
-        <div className="col-start-1  xs:col-start-1 sm:col-start-1  md:col-start-1 lg:flex lg:col-start-2  pr-4 pl-4  justify-end ">
-          <div className="flex percentege">{props.lineValue} % Compeleted</div>
-        </div>
-        <div className="col-span-full pr-4 pl-4">
-          <ProgressLine value={props.lineValue} />
-        </div>
-        <div className="col-start-1 col-end-1 sm:col-start-1 pr-4 pl-4">
-          <span>
-            Course Name: <span className="course-name">{props.courseName}</span>
-          </span>
-        </div>
-        <div className="col-start-1 xs:col-start-1 sm:col-start-1 lg:col-start-2 flex lg:justify-end  pr-4 pl-4">
-          <a href="#" className="resuming">
-            Resume Course
-          </a>
+        <div className="col-span-12 row-span-2">
+          <div
+            className="grid  p-4 gap-4"
+            style={{ gridTemplateRows: "auto auto" }}
+          >
+            <div className="col-span-full row-span-1">
+              <div className="col-start-1 col-end-3 flex justify-between">
+                <span className="status-normal">Latest Course</span>
+                <span className="percentage-normal">
+                  <strong>{props.lineValue} % Completed</strong>
+                </span>
+              </div>
+            </div>
+            {/* Progress Line */}
+            <div
+              className="col-span-full row-span-2"
+              style={{ marginTop: "-10px" }}
+            >
+              <ProgressLine value={props.lineValue} />
+            </div>
+            {/* Course Name */}
+            <div
+              className="col-span-full row-span-3 flex justify-between"
+              style={{ marginTop: "-25px" }}
+            >
+              <div className="flex">
+                <span className="name-normal">Course Name :</span>
+                <span className="name-data-normal">
+                  <strong>{props.courseName}</strong>
+                </span>
+              </div>
+              <a href="#" className="resume-course-max">
+                Resume Course
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </>
