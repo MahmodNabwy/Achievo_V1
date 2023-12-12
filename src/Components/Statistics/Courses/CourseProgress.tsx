0a3d0f58ffc354acbd8c5a1bcec7c12f48954c60
MahmodNabwy <mahmoud.ashraf@digitalhub.com.eg>
2023-12-12 10:24:28 +0200

    Move Min Courses Card Classes From Main Card To Courses sccs

--- src/Components/Statistics/Courses/CourseProgress.tsx
+++ src/Components/Statistics/Courses/CourseProgress.tsx
@@ -28,7 +28,7 @@ const CourseProgress = (props: CourseProgressProps) => {
 
   return props.isMinmized === true ? (
     <>
-      <div className="grid grid-cols-1 xs:grid-col-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-1 h-full card-section">
+      <div className="grid grid-cols-1 xs:grid-col-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-1 h-full courses-section">
         <MainCard
           icon={coursesIcon}
           title="Courses"
