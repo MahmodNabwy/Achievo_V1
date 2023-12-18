c224b5bc70417ffeebe3c42f6c24c1d0ae9b976d
MahmodNabwy <mahmoud.ashraf@digitalhub.com.eg>
2023-12-18 16:25:42 +0200

    Finish Courses Rectangle Card (Start Type)

--- src/Pages/Courses-Cards/CoursesCards.tsx
+++ src/Pages/Courses-Cards/CoursesCards.tsx
@@ -6,6 +6,7 @@ import rectangleIcon from "./Assets/images/Paper.svg";
 import arrowIcon from "./Assets/images/PaperPlus.svg";
 import fourCirclesIcon from "./Assets/images/four-circle.svg";
 import courseCover from "./Assets/images/Rectangle 531.png";
+import cover from "./Assets/images/Rectangle 530.png";
 type CoursesCardsProps = {};
 export const CoursesCards = (props: CoursesCardsProps) => {
   const header = {
@@ -15,111 +16,41 @@ export const CoursesCards = (props: CoursesCardsProps) => {
   return (
     <div className="courses-container">
       <ModuleHeader navItems={header.data} icons={header.icons} />
-      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-4 courses-grid">
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 courses-grid">
         {/* Re Take Square Card */}
-        <div className="card-square">
-          <div
-            style={{
-              borderLeft: "26px solid #60B527",
-              borderRadius: "12px",
-              height: "100%",
-            }}
-          >
-            <span className="course-name">Security Awareness Training</span>
-            <div className="flex justify-between">
-              <span className="create-date">Created Date: 05-08-2023</span>
-              <div>
-                <StarsRating />
-              </div>
-            </div>
-            <div>
-              <img src={courseCover} className="w-full" alt="course-cover" />
-            </div>
-            <div style={{ marginTop: "-13px" }}>
-              <LineProgress value={100} />
-            </div>
-            <div className="course-information">
-              <div className="deparment">IT</div>
-              <div className="course-status">
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    paddingRight: "10px",
-                  }}
-                >
-                  <div className="badge"></div>
-                  <span className="percentage">100%</span>
-                </div>
-                <div className="retake">Retake</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* Resume Square Card */}
-        <div className="card-square">
-          <div
-            style={{
-              borderLeft: "26px solid #E1CD17",
-              borderRadius: "12px",
-              height: "100%",
-            }}
-          >
-            <span className="course-name">Security Awareness Training</span>
-            <div className="flex justify-between">
-              <span className="create-date">Created Date: 05-08-2023</span>
-              <div>
-                <StarsRating />
-              </div>
-            </div>
-            <div>
-              <img src={courseCover} className="w-full" alt="course-cover" />
-            </div>
-            <div style={{ marginTop: "-13px" }}>
-              <LineProgress value={72} />
-            </div>
-            <div className="course-information">
-              <div className="deparment">IT</div>
-              <div className="course-status">
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    paddingRight: "10px",
-                  }}
-                >
-                  <div className="badge"></div>
-                  <span className="percentage">100%</span>
-                </div>
-                <div className="resume">Resume</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* Start Square Card */}
-        <div className="card-square">
+        <div className="card-rectangle">
           <div
             style={{
               borderLeft: "26px solid #178CE1",
-              borderRadius: "12px",
+              borderRadius: "7px",
               height: "100%",
+              justifyContent: "space-between",
             }}
           >
-            <span className="course-name">Security Awareness Training</span>
-            <div className="flex justify-between">
-              <span className="create-date">Created Date: 05-08-2023</span>
-              <div>
-                <StarsRating />
+            <div className="flex justify-between pl-4">
+              <div className="pt-4">
+                <span className="course-name">Security Awareness Training</span>
+                <div className="flex justify-between">
+                  <span className="create-date">Created Date: 05-08-2023</span>
+                  <div>
+                    <StarsRating />
+                  </div>
+                </div>
               </div>
+              <img
+                src={cover}
+                className="course-rectangle-cover"
+                alt="course-cover"
+              />
             </div>
-            <div>
-              <img src={courseCover} className="w-full" alt="course-cover" />
+            <div style={{ marginTop: "-17px" }}>
+              <LineProgress value={100} />
             </div>
-            <div style={{ marginTop: "-13px" }}>
-              <LineProgress value={0} />
-            </div>
-            <div className="course-information">
-              <div className="deparment">HR</div>
+            <div
+              className="flex justify-between"
+              style={{ marginTop: "-7px", alignItems: "center" }}
+            >
+              <div className="deparment">IT</div>
               <div className="course-status">
                 <div
                   style={{
@@ -131,7 +62,7 @@ export const CoursesCards = (props: CoursesCardsProps) => {
                   <div className="badge"></div>
                   <span className="percentage">0%</span>
                 </div>
-                <div className="Start">Start</div>
+                <div className="start-rectangle">Start</div>
               </div>
             </div>
           </div>
