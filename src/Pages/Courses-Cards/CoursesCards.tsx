8e4e45ba7cdef0030283aa292892c39f53162a02
MahmodNabwy <mahmoud.ashraf@digitalhub.com.eg>
2023-12-18 14:10:31 +0200

    Square Couses Cards Figure v1

--- src/Pages/Courses-Cards/CoursesCards.tsx
+++ src/Pages/Courses-Cards/CoursesCards.tsx
@@ -5,6 +5,7 @@ import { ModuleHeader } from "../../Components/ModuleHeader/ModuleHeader";
 import rectangleIcon from "./Assets/images/Paper.svg";
 import arrowIcon from "./Assets/images/PaperPlus.svg";
 import fourCirclesIcon from "./Assets/images/four-circle.svg";
+import courseCover from "./Assets/images/Rectangle 531.png";
 type CoursesCardsProps = {};
 export const CoursesCards = (props: CoursesCardsProps) => {
   const header = {
@@ -14,157 +15,74 @@ export const CoursesCards = (props: CoursesCardsProps) => {
   return (
     <div className="courses-container">
       <ModuleHeader navItems={header.data} icons={header.icons} />
-      <div className="grid grid-cols-1 gap-4 courses-grid">
-        {/* Retake Course */}
-        <div className="CoursesCards">
-          <div>
-            <div className="box">
-              <div className="rectangle-retake" />
-            </div>
-          </div>
-          <div style={{ width: "100%" }}>
-            <div>
-              <div className="data-container">
-                <div>
-                  <span className="course-name">
-                    Security Awareness Training
-                  </span>
-                  <div style={{ display: "flex" }}>
-                    <span className="create-date">
-                      Created Date: 05-08-2023
-                    </span>
-                    <StarsRating />
-                  </div>
-                </div>
+      {/* Rectangle Figure */}
 
-                <div className="frame"></div>
+      {/* Square Figure */}
+
+      <div className="grid grid-col-3 gap-6 pb-4 courses-grid">
+        <div className="col-start-1">
+          <div className="card-square flex justify-between">
+            <div>
+              <div className="square-box">
+                <div className="square-retake" />
               </div>
             </div>
-            {/* Progress Div */}
-            <div
-              style={{
-                marginTop: "-13px",
-              }}
-            >
-              <LineProgress value={100} />
-            </div>
-            <div className="course-information">
-              <div className="deparment">IT</div>
-              <div className="course-status">
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    paddingRight: "10px",
-                  }}
-                >
-                  <div className="badge"></div>
-                  <span className="percentage">100%</span>
-                </div>
-                <div className="retake">Retake</div>
+            <div className="pl-4">
+              <span className="course-name">Security Awareness Training</span>
+              <div style={{ display: "flex" }}>
+                <span className="create-date">Created Date: 05-08-2023</span>
+                <StarsRating />
+              </div>
+              <div className="w-full">
+                <img src={courseCover} alt="course-cover" />
               </div>
             </div>
           </div>
         </div>
-        {/* Resume Course */}
-        <div className="CoursesCards">
-          <div>
-            <div className="box">
-              <div className="rectangle-resume" />
-            </div>
-          </div>
-          <div style={{ width: "100%" }}>
+        <div className="col-start-2">
+          <div className="card-square flex justify-between">
             <div>
-              <div className="data-container">
-                <div>
-                  <span className="course-name">Communication Skills</span>
-                  <div style={{ display: "flex" }}>
-                    <span className="create-date">
-                      Created Date: 03-06-2023
-                    </span>
-                    <StarsRating />
-                  </div>
-                </div>
-
-                <div className="frame"></div>
+              <div className="square-box">
+                <div className="square-retake" />
               </div>
             </div>
-            {/* Progress Div */}
-            <div
-              style={{
-                marginTop: "-13px",
-              }}
-            >
-              <LineProgress value={72} />
-            </div>
-            <div className="course-information">
-              <div className="deparment">IT</div>
-              <div className="course-status">
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    paddingRight: "10px",
-                  }}
-                >
-                  <div className="badge"></div>
-                  <span className="percentage">72%</span>
-                </div>
-                <div className="resume">Resume</div>
+            <div className="pl-4">
+              <span className="course-name">Security Awareness Training</span>
+              <div style={{ display: "flex" }}>
+                <span className="create-date">Created Date: 05-08-2023</span>
+                <StarsRating />
+              </div>
+              <div className="w-full">
+                <img src={courseCover} alt="course-cover" />
               </div>
             </div>
           </div>
         </div>
-
-        {/*  Start Course */}
-        <div className="CoursesCards">
-          <div>
-            <div className="box">
-              <div className="rectangle-start" />
-            </div>
-          </div>
-          <div style={{ width: "100%" }}>
+        <div className="col-start-3">
+          {" "}
+          <div className="card-square flex justify-between">
             <div>
-              <div className="data-container">
-                <div>
-                  <span className="course-name">Cloud Security</span>
-                  <div style={{ display: "flex" }}>
-                    <span className="create-date">
-                      Created Date: 22-05-2023
-                    </span>
-                    <StarsRating />
-                  </div>
-                </div>
-
-                <div className="frame"></div>
+              <div className="square-box">
+                <div className="square-retake" />
               </div>
             </div>
-            {/* Progress Div */}
-            <div
-              style={{
-                marginTop: "-13px",
-              }}
-            >
-              <LineProgress value={0} />
-            </div>
-            <div className="course-information">
-              <div className="deparment">IT</div>
-              <div className="course-status">
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    paddingRight: "10px",
-                  }}
-                >
-                  <div className="badge"></div>
-                  <span className="percentage">0%</span>
-                </div>
-                <div className="Start">Start</div>
+            <div className="pl-4">
+              <span className="course-name">Security Awareness Training</span>
+              <div style={{ display: "flex" }}>
+                <span className="create-date">Created Date: 05-08-2023</span>
+                <StarsRating />
+              </div>
+              <div className="w-full">
+                <img src={courseCover} alt="course-cover" />
               </div>
             </div>
           </div>
         </div>
+      </div>
+      <div className="grid grid-col-3 gap-6">
+        <div className="col-start-1">111</div>
+        <div className="col-start-2">111</div>
+        <div className="col-start-3">111</div>
       </div>
     </div>
   );
