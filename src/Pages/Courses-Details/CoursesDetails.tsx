55c8a5a72c6fddf7074c951ded1e9dc0c84a0dd9
MahmodNabwy <mahmoud.ashraf@digitalhub.com.eg>
2023-12-20 16:33:34 +0200

    Build Course Details Page

--- src/Pages/Courses-Details/CoursesDetails.tsx
+++ src/Pages/Courses-Details/CoursesDetails.tsx
@@ -2,16 +2,16 @@ import { GoBackHeader } from "../../Components/GoBackHeader/GoBackHeader";
 import { StarsRating } from "../../Components/StarsRating/StarsRating";
 import courseCover from "../Courses-Cards/Assets/images/Rectangle 531.png";
 import ProgressLine from "../../Components/Statistics/Courses/ProgressLine";
-
+import videoIcon from "./Assets/Icons/video.svg";
 import "./CorusesDetails.scss";
 import { LineProgress } from "../../Components/LineProgress/LineProgress";
 export const CoursesDetails = () => {
   return (
     <div className="CoursesDetails">
       <GoBackHeader />
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 lg:grid-cols-2 gap-9 pr-10 pl-10">
         {/* Course-Information */}
-        <div className="col-start-1">
+        <div className="sm:col-start-1 lg:col-start-1">
           <div className="course-informations">
             <span className="course-name">Security Awareness Training</span>
             <span className="created-date">Created Date: 05-05-2023</span>
@@ -90,17 +90,322 @@ export const CoursesDetails = () => {
             </div>
           </div>
         </div>
-        <div className="col-start-2">
+
+        {/* Course Cover */}
+        <div className="sm:col-start-1 lg:col-start-2">
           <div className="course-cover">
             <img src={courseCover} className="cover" alt="cover" />
           </div>
         </div>
+        {/* Line Progress */}
         <div className="col-span-full">
           <LineProgress value={100} />
         </div>
+        {/* Expand All , Collapse All */}
+        <div className="col-span-full">
+          <div className="flex pl-6 gap-8 mt-4">
+            {/* Expand All */}
+            <div className="flex items-center gap-4">
+              <div className="option">Expand All</div>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                >
+                  <path
+                    d="M10.0312 7.05249V13.0092"
+                    stroke="black"
+                    stroke-width="1.5"
+                    stroke-miterlimit="10"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M13.0131 10.031H7.0498"
+                    stroke="black"
+                    stroke-width="1.5"
+                    stroke-miterlimit="10"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M1.91699 10.0309C1.91699 3.94592 3.94616 1.91675 10.0312 1.91675C16.1162 1.91675 18.1453 3.94592 18.1453 10.0309C18.1453 16.1159 16.1162 18.1451 10.0312 18.1451C3.94616 18.1451 1.91699 16.1159 1.91699 10.0309Z"
+                    stroke="black"
+                    stroke-width="1.5"
+                    stroke-miterlimit="10"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+            </div>
+            {/* Collapse All */}
+            <div className="flex items-center gap-4">
+              <div className="option">Collapse All</div>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                >
+                  <path
+                    d="M13.0131 10.031H7.0498"
+                    stroke="black"
+                    stroke-width="1.5"
+                    stroke-miterlimit="10"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M1.91699 10.0309C1.91699 3.94592 3.94616 1.91675 10.0312 1.91675C16.1162 1.91675 18.1453 3.94592 18.1453 10.0309C18.1453 16.1159 16.1162 18.1451 10.0312 18.1451C3.94616 18.1451 1.91699 16.1159 1.91699 10.0309Z"
+                    stroke="black"
+                    stroke-width="1.5"
+                    stroke-miterlimit="10"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Course Sessions */}
+        <div className="col-start-1 pl-4">
+          <div className="accordion-group accordion-group-bordered mt-4">
+            <div className="accordion">
+              <input
+                type="checkbox"
+                id="toggle-7"
+                className="accordion-toggle"
+              />
+              {/* Header */}
+              <div className="flex items-center pl-4 gap-4 session-header">
+                <label htmlFor="toggle-7">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                  >
+                    <path
+                      d="M10.0312 7.05249V13.0092"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M13.0131 10.031H7.0498"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M1.91699 10.0309C1.91699 3.94592 3.94616 1.91675 10.0312 1.91675C16.1162 1.91675 18.1453 3.94592 18.1453 10.0309C18.1453 16.1159 16.1162 18.1451 10.0312 18.1451C3.94616 18.1451 1.91699 16.1159 1.91699 10.0309Z"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </label>
+                <label htmlFor="toggle-7" className="accordion-title">
+                  Importance of Security
+                </label>
+              </div>
+              {/* Content */}
+              <div className="accordion-content text-content2">
+                <div className="min-h-0 session-content">
+                  This module will introduce you to security awareness basics.
+                  We will discuss the importance of and review the components of
+                  security. We will conclude the module by examining hackers and
+                  what they target.
+                  <div className="course-session-content">
+                    <div className="flex justify-between">
+                      {/* Video */}
+                      <div className="flex gap-4">
+                        <div>
+                          <img src={videoIcon} alt="video" />
+                        </div>
+                        <div className="grid video-info">
+                          <span className="video-title">
+                            Importance of Security
+                          </span>
+                          <span className="video-duration">Video - 13 Min</span>
+                        </div>
+                      </div>
+                      <div className="letsStart">Let’s Start</div>
+                    </div>
 
-        <div className="col-start-1 pl-4">Course Sessions</div>
-        <div className="col-start-2">About This Course</div>
+                    <div className="flex justify-between mt-4">
+                      {/* Video */}
+                      <div className="flex gap-4">
+                        <div>
+                          <img src={videoIcon} alt="video" />
+                        </div>
+                        <div className="grid video-info">
+                          <span className="video-title">
+                            Importance of Security
+                          </span>
+                          <span className="video-duration">Video - 13 Min</span>
+                        </div>
+                      </div>
+                      <div className="letsStart">Let’s Start</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="accordion-group accordion-group-bordered mt-4">
+            <div className="accordion">
+              <input
+                type="checkbox"
+                id="toggle-8"
+                className="accordion-toggle"
+              />
+              <div className="flex items-center pl-4 gap-4 session-header">
+                <label htmlFor="toggle-8">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                  >
+                    <path
+                      d="M10.0312 7.05249V13.0092"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M13.0131 10.031H7.0498"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M1.91699 10.0309C1.91699 3.94592 3.94616 1.91675 10.0312 1.91675C16.1162 1.91675 18.1453 3.94592 18.1453 10.0309C18.1453 16.1159 16.1162 18.1451 10.0312 18.1451C3.94616 18.1451 1.91699 16.1159 1.91699 10.0309Z"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </label>
+                <label htmlFor="toggle-8" className="accordion-title">
+                  Importance of Security
+                </label>
+              </div>
+              <div className="accordion-content text-content2">
+                <div className="min-h-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+                  necessitatibus iusto laborum autem placeat aspernatur
+                  inventore eius deleniti reprehenderit? Numquam commodi totam
+                  mollitia quod
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="accordion-group accordion-group-bordered mt-4">
+            <div className="accordion">
+              <input
+                type="checkbox"
+                id="toggle-9"
+                className="accordion-toggle"
+              />
+              <div className="flex items-center pl-4 gap-4 session-header">
+                <label htmlFor="toggle-9">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                  >
+                    <path
+                      d="M10.0312 7.05249V13.0092"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M13.0131 10.031H7.0498"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M1.91699 10.0309C1.91699 3.94592 3.94616 1.91675 10.0312 1.91675C16.1162 1.91675 18.1453 3.94592 18.1453 10.0309C18.1453 16.1159 16.1162 18.1451 10.0312 18.1451C3.94616 18.1451 1.91699 16.1159 1.91699 10.0309Z"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </label>
+                <label htmlFor="toggle-9" className="accordion-title">
+                  Importance of Security
+                </label>
+              </div>
+              <div className="accordion-content text-content2">
+                <div className="min-h-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+                  necessitatibus iusto laborum autem placeat aspernatur
+                  inventore eius deleniti reprehenderit? Numquam commodi totam
+                  mollitia quod
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* About The Course */}
+        <div className="col-start-2">
+          <div className="about-course">
+            <span className="about-title">About this Course</span>
+            <div className="content">
+              The security of an organization is of the utmost importance and
+              every member of the organization's staff plays a vital role in
+              defending against cyber threats. One of the best ways to protect
+              the organization is to institute a company-wide security-awareness
+              training initiative. This course is a complete foundational
+              security awareness training program that covers a wide array of
+              topics for nearly every type of end-user and learner level. The
+              content is designed to allow organizations to be able to provide a
+              comprehensive training program to help them protect their
+              information assets against threats. This training lasts
+              approximately 2 hours and was designed to be engaging and is based
+              on real scenarios staff may face. The training is modular and does
+              not have to be completed in one sitting. Topics included in this
+              course are as follows: • Importance of Security • Data and Account
+              Security • Passwords • Networking and Mobile Security • Malware •
+              Social Engineering
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
