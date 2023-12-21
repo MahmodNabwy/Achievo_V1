de873a7f823b8e370486212d4a35ca2229bbbbd4
MahmodNabwy <mahmoud.ashraf@digitalhub.com.eg>
2023-12-21 13:03:21 +0200

    Build Login Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { Sidebar } from "./Components/Sidebar/Sidebar";
 import { Circles } from "./Pages/Dashboard-Circles/Circles";
 import { Cards } from "./Pages/Dashboard-Cards/Cards";
 import { Layout } from "./app-layout/Layout";
+import { Login } from "./Pages/Login/Login";
 const App = () => {
   return (
     <div>
-      <Layout />
+      {/* <Layout /> */}
+      <Login />
     </div>
   );
 };
